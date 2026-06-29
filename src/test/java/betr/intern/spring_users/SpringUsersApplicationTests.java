536bb8d4c131a59ce129167e3c4264543dea0031
denisa.caruntupitchtech.ro <caruntudenisa12@gmail.com>
2026-06-29 15:57:42 +0300

    Initial commit for this task

---
+++ src/test/java/betr/intern/spring_users/SpringUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package betr.intern.spring_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
