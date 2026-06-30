370f40575a05cf9a385c6d6f3289296b68259152
caruntudenisa <118226557+caruntudenisa@users.noreply.github.com>
2026-06-30 11:11:05 +0300

    feat (#1): Spring Boot Project Implementation, User Class Implemented
    * Initial commit for this task
    
    * feat(1): Implementation of User class
    
    * Add User entity
    
    * feat(): implementation of the User Class

---
+++ src/main/java/betr/intern/spring_users/SpringUsersApplication.java
@@ -0,0 +1,13 @@
+package betr.intern.spring_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsersApplication.class, args);
+	}
+
+}
